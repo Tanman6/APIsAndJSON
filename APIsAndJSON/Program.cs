838a2697423f4d096a8e3a77944639cc8c4c4022
Tanman6 <tanmansyx@protonmail.com>
2022-03-16 11:24:19 -0500

    init commit

---
+++ APIsAndJSON/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeWest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var quote = new QuoteGen(client);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Kanye: {quote.Kanye()}");
+                Console.WriteLine($"Ron Swanson: {quote.RonSwanson()}");
+                Console.WriteLine();
+
+            }
+
+        }
+
+
+    }
+}
+
+         
---
+++ APIsAndJSON/QuoteGen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeWest
+{
+    public class QuoteGen
+    {
+        private HttpClient _client;
+
+        public QuoteGen(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public string Kanye()
+        {
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+        public string RonSwanson()
+        {
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = _client.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            return ronQuote.ToString();
+        }
+    }
+}
